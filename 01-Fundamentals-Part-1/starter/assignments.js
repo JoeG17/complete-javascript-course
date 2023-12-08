e09a09c9feb89290355a36dc7729596f2f980461
JoeG17 <josephgeli@hotmail.com>
2023-12-08 22:03:15 +0000

    Created new branch

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,27 @@
+let country = 'philippines';
+let continent = 'asia';
+let population = '30000000';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// block 2
+let isIsland = false;
+let language = '';
+console.log(typeof country);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+
+
+//block 3
+const language = 'tagalog';
+
+
+// Lecture 4
+
+
+country = country / 2;
+console.log(country + 1);
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+
+let js = 'Amazing';
+if (js === 'Amazing') alert('JavaScript is Amazing');
+
+48 + 10 + 10 - 10;
+console.log(48+10+10);``
