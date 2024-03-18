e9f7048dcf4b98657887b3ef0d59a9444228b237
JoeG17 <josephgeli@hotmail.com>
2024-03-17 19:02:49 -0700

    jgtest

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,33 @@
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+/* Write your code below. Good luck! 🙂 */
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+let tips = [];
+
+let totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+
+  totals.push(bills[i] + tip);
+}
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
+
+const testName = 'John';
+
+console.log('testt1est1');
