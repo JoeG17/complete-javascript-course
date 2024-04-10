31788c86ed13f5beb9206980b16f35d50371eb29
Joseph Geli <joeg@mdepeaceqsoftware.onmicrosoft.com>
2024-04-10 14:14:16 -0700

    1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,12 +19,16 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = 20;
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
@@ -34,9 +38,9 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number guessed!';
+    displayMessage('No number guessed!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct!!!';
+    displayMessage('Correct!!!');
     if (score > highScore) {
       document.querySelector('.highscore').textContent = score;
       highScore = score;
@@ -44,21 +48,18 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too High!!!';
+  } 
+  // Incorrect Guess
+  else if (guess !== secretNumber) {
+
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!!!' : 'Too Low!!!');
       score -= 1;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Loser! Try Again!';
+      displayMessage('Loser! Try Again!');
     }
-  } else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too Low!!!';
-      score -= 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Loser! Try Again!';
-    }
-  }
+
+  } 
 });
+  
